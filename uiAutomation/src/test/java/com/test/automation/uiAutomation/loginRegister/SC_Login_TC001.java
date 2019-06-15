ceed2ecdca738ff2b4ddf5167c044f31119c14be
admin <admin@192.168.0.106>
2019-06-15 11:51:00 +0530

    First Commit

---
+++ uiAutomation/src/test/java/com/test/automation/uiAutomation/loginRegister/SC_Login_TC001.java
@@ -0,0 +1,56 @@
+package com.test.automation.uiAutomation.loginRegister;
+
+import org.testng.annotations.Test;
+
+import com.test.automation.uiAutomation.testBase.TestBase;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class SC_Login_TC001 extends TestBase {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() throws IOException {
+	  init();
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}

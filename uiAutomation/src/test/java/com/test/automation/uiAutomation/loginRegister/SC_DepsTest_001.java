597aa22d860fc06b270f67c4c959bebc13649d38
sambeet nayak <sambeetnayak3334@gmail.com>
2019-06-18 13:07:25 +0530

    add new tests

---
+++ uiAutomation/src/test/java/com/test/automation/uiAutomation/loginRegister/SC_DepsTest_001.java
@@ -0,0 +1,56 @@
+package com.test.automation.uiAutomation.loginRegister;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SC_DepsTest_001 {
+	 @Test(groups = "pre-tests")
+	  public void Deps001() {
+		 System.out.println("Test deps 001 test 1");
+		 assertEquals(true, false);
+	  }
+	 
+	 @Test(groups = "pre-tests2")
+	  public void Deps002() {
+		 System.out.println("Test deps 001 test 2");
+		 assertEquals(true, true);
+		 
+	  }
+	 
+	 @Test
+	  public void Deps003() {
+		 System.out.println("Test deps 001 test 3");
+		 
+	  }
+	  @BeforeMethod
+	  public void beforeMethod() {
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+	  }
+
+	  @BeforeClass
+	  public void beforeClass() {
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+	  }
+
+	  @BeforeTest
+	  public void beforeTest() {
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+	  }
+
+}
---
+++ uiAutomation/src/test/java/com/test/automation/uiAutomation/loginRegister/SC_DepsTest_002.java
@@ -0,0 +1,48 @@
+package com.test.automation.uiAutomation.loginRegister;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class SC_DepsTest_002 {
+  @Test(dependsOnGroups = {"pre-tests","pre-tests2"})
+  public void Deps003() {
+	  System.out.println("Test deps 002 test 1");
+	  
+  }
+  @Test
+  public void Deps004() {
+	  System.out.println("Test deps 002 test 2");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
